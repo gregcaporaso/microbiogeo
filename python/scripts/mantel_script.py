04af471d263e03ee233a7fa744658480f03d6de3
Logan Knecht <loganknecht@gmail.com>
2012-03-19 03:57:17 +0000

    Fixed spacing issue for output

--- python/scripts/mantel_script.py
+++ python/scripts/mantel_script.py
@@ -62,10 +62,8 @@ def main():
     input_dm_fps = opts.input_dms.split(',')
 
     output_f = open(opts.output_fp,'w')
-    output_f.write(comment)
-    #output_f.write('DM1\tDM2\tNumber of entries\tMantel p-value\n')
 
-    #this is where the headin information is added, it accounts for the spacing between file names for the first two elements DM1 and DM2, but it doesn't fix the spacing between the actual number values
+    #this is where the heading information is added, it accounts for the spacing between file names for the first two elements DM1 and DM2, but it doesn't fix the spacing between the actual number values
     output_f.write('DM1')
 
     num_of_first_spaces = len(input_dm_fps[0])
@@ -100,52 +98,40 @@ def main():
                 output_f.write('%s\t%s\t%d\tToo few samples\n' % (fp1,fp2,len(dm1_labels)))
                 continue
 
-            #m = Mantel(dm1, dm2, num_iterations)
-            #probably better to just pass in the fp1 and fp2 here in the constructor
             m = Mantel(dm1, dm2, num_iterations)
 
     resultsDict = {}
 
     p = m.runAnalysis()
-
     p_str = format_p_value_for_num_iters(p,num_iterations)
-    output_str = ('%s\t%s\t%d\t%s\n' % (input_dm_fps[0], input_dm_fps[1], len(dm1_labels),p_str))
+    #output_str = ('%s\t%s\t%d\t%s\n' % (input_dm_fps[0], input_dm_fps[1], len(dm1_labels),p_str))
     #resultsDict['Results':('%s\t%s\t%d\t%s\n' % (fp1, fp2, len(dm1_labels),p_str))]
-    resultsDict['Results'] = output_str
 
-    for line in  resultsDict['Results']:
-        #easy way to output the entries
-        #output_f.write(m_output[line])
+    resultsDict['Results'] = p_str
+    resultsDict['DM1'] = input_dm_fps[0] 
+    resultsDict['DM2'] = input_dm_fps[1] 
+    resultsDict['sampleSize'] = len(dm1_labels)
+    resultsDict['pvalue'] = p_str
 
-        #hard way to output entries on a per item basis, modify this to alter spacing for values
-        #I don't know why I didn't just use the above line because it's easier, just it doesn't handle white spacing like it should, so I wrote all the code below to handle the case for the third column so that the number is always space correctly creating a better sense of readability with the output.
-        items = line.split("\t")
-        tracker = 0
-        for item in items:
-            #This was grabbed from stack overflow in order to help with formatting...
-            #http://stackoverflow.com/questions/4703390/how-to-extract-a-floating-number-from-a-string-in-python
-            reFind = re.findall(r"[-+]?\d*\.\d+|\d+", item)
-            if(len(reFind) > 0):
-                spaces_to_add = 0
-                if(tracker == 2):
-                    spaces_to_add = len(num_of_entries_column_header) - len(item)
-                    spaces = ""
-                    if(spaces_to_add > 0):
-                        while(spaces_to_add > 0):
-                            spaces = spaces + " "
-                            spaces_to_add = spaces_to_add - 1
-                    output_f.write(item)
-                    output_f.write(spaces)
-                else:
-                    output_f.write(item)
-            else:
-                output_f.write(item)
+    output_f.write(resultsDict['DM1'])
+    output_f.write("\t")
 
-            if(tracker < len(items)-1):
-                output_f.write("\t")
+    output_f.write(resultsDict['DM2'])
+    output_f.write("\t")
 
-            tracker = tracker + 1
+    #fixes space issues for formatting
+    third_word_spaces_needed = len("Number of entries") - len(str(resultsDict['sampleSize']))
+    third_word_spaces = "" 
+    while(third_word_spaces_needed > 0):
+        third_word_spaces_needed = third_word_spaces_needed - 1
+        third_word_spaces = third_word_spaces + " "
+    output_f.write(str(resultsDict['sampleSize']))
+    output_f.write(third_word_spaces)
+    output_f.write("\t")
 
+    output_f.write(resultsDict['pvalue'])
+
+    output_f.write("\n")
     output_f.close()
 
 if __name__ == "__main__":
