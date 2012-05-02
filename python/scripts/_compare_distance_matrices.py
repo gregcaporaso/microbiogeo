eedde1e6bda91e31732923aa3b2911b967ed33c1
Michael Dwan <michael.dwan@gmail.com>
2012-05-02 10:49:15 +0000

    Reformatted _compare_distance_matrices.py. No longer extends beyond 80 columns.

--- python/scripts/_compare_distance_matrices.py
+++ python/scripts/_compare_distance_matrices.py
@@ -13,16 +13,13 @@ __status__ = "Development"
 
 
 from os import path
-
 from cogent.util.misc import create_dir
-
 from qiime.parse import parse_distmat, fields_to_dict
 from qiime.format import format_p_value_for_num_iters
 from qiime.util import (parse_command_line_parameters,
                         get_options_lookup,
                         make_compatible_distance_matrices,
                         make_option)
-
 from python.qiime.parse import DistanceMatrix
 from python.qiime.stats import Mantel, MantelCorrelogram, \
                               PartialMantel
@@ -30,42 +27,90 @@ from python.qiime.stats import Mantel, MantelCorrelogram, \
 options_lookup = get_options_lookup()
 
 script_info = {}
-script_info['brief_description'] = "Script for computing Mantel correlations between sets of distance matrices"
-script_info['script_description'] = """The script compare_distance_matrices.py compares two or more distance/dissimilarity matrices.
+script_info['brief_description'] = "Script for computing Mantel correlations \
+                                    between sets of distance matrices"
+script_info['script_description'] = """The script compare_distance_matrices.py \
+compares two or more distance/dissimilarity matrices.
 
-Mantel will test the correlation between two matrices. The data often represents the "distance" between objects.
+Mantel will test the correlation between two matrices. The data \
+often represents the "distance" between objects.
 
-partial Mantel is a first-order correlation analysis that utilizes three distance(dis-similarity) matrices. This builds on the simple Mantel which is a procedure that tests the hypothesis that distances between the objects within a given matrix are linearly independent of the distances withing those same objects in a separate matrix. It builds on the simple Mantel by adding a third "control" matrix.l
+partial Mantel is a first-order correlation analysis that \
+utilizes three distance(dis-similarity) matrices. This builds \
+on the simple Mantel which is a procedure that tests the \
+hypothesis that distances between the objects within a given \
+matrix are linearly independent of the distances withing those \
+same objects in a separate matrix. It builds on the simple Mantel \
+by adding a third "control" matrix.l
 
-Mantel correlogram is a plot of distance classes versus Mantel statistics. Briefly, an ecological distance matrix (e.g. UniFrac distance matrix) and a second distance matrix (e.g. spatial distances, pH distances, etc.) are provided to the script. The second distance matrix has its distances split into a number of distance classes (the number of classes is determined by Sturge's rule). A Mantel test is run over these distance classes versus the ecological distance matrix. The Mantel statistics obtained from each of these tests is then plotted in a correlogram. A filled-in point on the plot indicates that the Mantel statistic was statistically significant (you may provide what alpha to use).
+Mantel correlogram is a plot of distance classes versus Mantel \
+statistics. Briefly, an ecological distance matrix (e.g. UniFrac \
+distance matrix) and a second distance matrix (e.g. spatial distances, \
+pH distances, etc.) are provided to the script. The second distance \
+matrix has its distances split into a number of distance classes \
+(the number of classes is determined by Sturge's rule). A Mantel \
+test is run over these distance classes versus the ecological distance \
+matrix. The Mantel statistics obtained from each of these tests is \
+then plotted in a correlogram. A filled-in point on the plot indicates \
+that the Mantel statistic was statistically significant \
+(you may provide what alpha to use).
 """
 script_info['script_usage'] = []
-script_info['script_usage'].append(("Partial Mantel","Perform a partial Mantel test on two distance matrices, using a third matrix as a control. Also run 100 permutations.", "%prog --method partial_mantel -i weighted_unifrac_dm.txt,unweighted_unifrac_dm.txt -c PH_dm.txt -o mantel_out -n 100"))
-script_info['script_usage'].append(("Mantel","Perform Mantel test on all pairs of four distance matrices, including 1000 Monte Carlo iterations. Write the output to mantel_out.txt.","%prog --method mantel -i weighted_unifrac_dm.txt,unweighted_unifrac_dm.txt,weighted_unifrac_even100_dm.txt,unweighted_unifrac_even100_dm.txt -o mantel_out -n 1000"))
-script_info['script_usage'].append(("Mantel Correlogram","This example computes a Mantel correlogram on two distance matrices using 999 permutations in each Mantel test. Output is written to the mantel_output directory.", "%prog -i unweighted_unifrac_dm.txt,weighted_unifrac_dm.txt -o mantel_output "
+script_info['script_usage'].append(("Partial Mantel",
+"Perform a partial Mantel test on two distance matrices, "
+"using a third matrix as a control. Also run 100 permutations.",
+"%prog --method partial_mantel -i "
+"weighted_unifrac_dm.txt,unweighted_unifrac_dm.txt -c PH_dm.txt "
+"-o mantel_out -n 100"))
+script_info['script_usage'].append(("Mantel",
+"Perform Mantel test on all pairs of four distance matrices, "
+"including 1000 Monte Carlo iterations. Write the output to mantel_out.txt.",
+"%prog --method mantel "
+"-i weighted_unifrac_dm.txt,unweighted_unifrac_dm.txt,"
+"weighted_unifrac_even100_dm.txt,unweighted_unifrac_even100_dm.txt "
+"-o mantel_out -n 1000"))
+script_info['script_usage'].append(("Mantel Correlogram",
+"This example computes a Mantel correlogram on two distance matrices "
+"using 999 permutations in each Mantel test. Output is written to the "
+"mantel_output directory.",
+"%prog -i unweighted_unifrac_dm.txt,weighted_unifrac_dm.txt -o mantel_output "
 "-n 999"))
 
 
-script_info['output_description']= """Mantel: One file is created containing the Mantel 'r' statistic and p-value.
+script_info['output_description']= """Mantel: One file is created \
+containing the Mantel 'r' statistic and p-value.
 
-partial Mantel: One file is created in the output directory, which contains the partial Mantel statistic and p-value.
+partial Mantel: One file is created in the output directory, which \
+contains the partial Mantel statistic and p-value.
 
-Mantel Correlogram: Two files are created in the output directory: a text file containing information about the distance classes, their associated Mantel statistics and p-values, etc. and an image of the correlogram plot.
+Mantel Correlogram: Two files are created in the output directory: \
+a text file containing information about the distance classes, their \
+associated Mantel statistics and p-values, etc. and an image of the \
+correlogram plot.
 """
 script_info['required_options'] = [\
  # All methods use these
-make_option('--method', help='Matrix Correletion method to be executed. Valid options: [mantel, partial_mantel, mantel_corr]', type='choice', choices=['mantel', 'partial_mantel', 'mantel_corr']),
- make_option('-i','--input_dms',help='the input distance matrices, comma-separated'),\
- make_option('-o','--output_dir',help='the output directory [default: %default]', default='.'),\
+make_option('--method',
+    help='Matrix Correletion method to be executed. Valid options: '
+    '[mantel, partial_mantel, mantel_corr]',
+    type='choice',
+    choices=['mantel', 'partial_mantel', 'mantel_corr']),
+ make_option('-i','--input_dms',
+    help='the input distance matrices, comma-separated'),\
+ make_option('-o','--output_dir',
+    help='the output directory [default: %default]', default='.'),\
 ]
 script_info['optional_options'] = [
  # All methods use these
- make_option('-n','--num_permutations',help='the number of iterations to perform',default=100,type='int'),
+ make_option('-n','--num_permutations',
+    help='the number of iterations to perform',default=100,type='int'),
  make_option('-s','--sample_id_map_fp',
     help='Map of original sample ids to new sample ids [default: %default]',
     default=None),
  # Standard Mantel specific, i.e., method == mantel
-make_option('-t','--tail_type',help='the type of tailed test to perform(1 or 2 tailed [default:%default]', default='two sided'),
+make_option('-t','--tail_type',
+    help='the type of tailed test to perform(1 or 2 tailed [default:%default]',
+    default='two sided'),
  # Mantel Correlogram specific, i.e., method == mantel_corr
 make_option('-a', '--alpha',
     help='the value of alpha to use when denoting significance in the '
@@ -81,7 +126,8 @@ make_option('-c', '--control_dm',
 ]
 script_info['version'] = __version__
 
-comment_mantel_pmantel = """# Number of entries refers to the number of rows (or cols)
+comment_mantel_pmantel = """# Number of entries refers to the number \
+of rows (or cols)
 # retained in each distance matrix after filtering the distance matrices
 # to include only those samples that were in both distance matrices.
 # p-value contains the correct number of significant digits.
@@ -105,7 +151,8 @@ def main():
         if not path.exists(opts.output_dir):
             create_dir(opts.output_dir)
     except:
-        option_parser.error("Could not create or access output directory specified with the -o option.")
+        option_parser.error("Could not create or access output directory "
+                            "specified with the -o option.")
 
     sample_id_map_fp = opts.sample_id_map_fp
     if sample_id_map_fp:
@@ -119,7 +166,10 @@ def main():
 
     if opts.method == 'mantel':
         output_f = open(path.join(opts.output_dir, "mantel_results.txt"), 'w')
-        #this is where the heading information is added, it accounts for the spacing between file names for the first two elements DM1 and DM2, but it doesn't fix the spacing between the actual number values
+        #this is where the heading information is added, it accounts
+        #for the spacing between file names for the first two elements
+        #DM1 and DM2, but it doesn't fix the spacing between the
+        #actual number values
         output_f.write(comment_corr)
         output_f.write('DM1')
 
@@ -151,15 +201,25 @@ def main():
 
         for i,fp1 in enumerate(input_dm_fps):
             for fp2 in input_dm_fps[i+1:]:
-                #this is a relatively benign looking line of code, but a lot is going on in the background that isn't being seen. For starters it's parsing distance matrice files and returning the infomration as a list in an array, then it has it made compatable and returned yet again as a pair of data with the informations being (sample ids, distant matrix data)
+                #this is a relatively benign looking line of code, but
+                #a lot is going on in the background that isn't
+                #being seen. For starters it's parsing distance matrice
+                #files and returning the infomration as a list in an
+                #array, then it has it made compatable and returned
+                #yet again as a pair of data with the informations
+                #being (sample ids, distant matrix data)
                 (dm1_labels, dm1), (dm2_labels, dm2) =\
                  make_compatible_distance_matrices(parse_distmat(open(fp1,'U')), parse_distmat(open(fp2,'U')), lookup=sample_id_map)
                 if len(dm1_labels) < 2:
-                    output_f.write('%s\t%s\t%d\tToo few samples\n' % (fp1,fp2,len(dm1_labels)))
+                    output_f.write('%s\t%s\t%d\tToo few samples\n' % (fp1,
+                                   fp2,len(dm1_labels)))
                     continue
 
-                #This takes in a distance matrix object for the dm1 and dm2 of Mantel
-                m = Mantel(DistanceMatrix(dm1, dm1_labels, dm1_labels), DistanceMatrix(dm2, dm2_labels, dm2_labels), opts.tail_type)
+                #This takes in a distance matrix object for the
+                #dm1 and dm2 of Mantel
+                m = Mantel(DistanceMatrix(dm1, dm1_labels, dm1_labels),
+                           DistanceMatrix(dm2, dm2_labels, dm2_labels),
+                           opts.tail_type)
 
                 resultsDict = {}
                 resultsDict = m(num_permutations)
@@ -176,7 +236,8 @@ def main():
                 output_f.write("\t")
 
                 #fixes space issues for formatting
-                third_word_spaces_needed = len("Number of entries") - len(dm1_labels)
+                third_word_spaces_needed = \
+                    len("Number of entries") - len(dm1_labels)
                 third_word_spaces = ""
                 while(third_word_spaces_needed > 0):
                     third_word_spaces_needed = third_word_spaces_needed - 1
@@ -197,23 +258,28 @@ def main():
 
         control_dm_fp = opts.control_dm
 
-        res_file = open(path.join(opts.output_dir, "mantel_partial_results.txt"), 'w')
+        res_file = open(path.join(opts.output_dir,
+                        "mantel_partial_results.txt"), 'w')
         res_file.write(comment_mantel_pmantel)
 
         # Output header to result file.
-        res_file.write('\nCM: %s\npermutations: %d\n' % (control_dm_fp, num_perms))
+        res_file.write('\nCM: %s\npermutations: %d\n' %
+                               (control_dm_fp, num_perms))
 
         res_file.write('\nDM1\tDM2\tMantel stat(r-val)\tp-val\t')
 
         for i,fp1 in enumerate(input_dm_fps):
             for fp2 in input_dm_fps[i+1:]:
-                (dm1_labels, dm1), (dm2_labels, dm2) = make_compatible_distance_matrices(
+                (dm1_labels, dm1), (dm2_labels, dm2) = \
+                    make_compatible_distance_matrices(
                     parse_distmat(open(fp1, 'U')),
                     parse_distmat(open(fp2, 'U')), lookup=sample_id_map)
 
-                (dm1_labels, dm1), (cdm_labels, cdm) = make_compatible_distance_matrices(
+                (dm1_labels, dm1), (cdm_labels, cdm) = \
+                    make_compatible_distance_matrices(
                     parse_distmat(open(fp1, 'U')),
-                    parse_distmat(open(control_dm_fp, 'U')), lookup=sample_id_map)
+                    parse_distmat(open(control_dm_fp, 'U')),
+                        lookup=sample_id_map)
 
 
                 # Construct a PartialMantel object.
@@ -225,7 +291,8 @@ def main():
                 res = pm(num_perms)
 
                 # Output statistic to result file.
-                res_file.write('\n%s\t%s\t%f\t%f' % (fp1,fp2,res['mantel_r'], res['mantel_p']))
+                res_file.write('\n%s\t%s\t%f\t%f' %
+                                (fp1,fp2,res['mantel_r'], res['mantel_p']))
         res_file.close()
 
     elif opts.method == 'mantel_corr':
@@ -250,22 +317,26 @@ def main():
 
         # Write header info to the results file.
         results_f.write('Number of entries: %d(per matrix)\nNumber of '
-            'permutations: %d\nAlpha: %s\n' % (len(dm_labels), num_perms, alpha))
+            'permutations: %d\nAlpha: %s\n' %
+                    (len(dm_labels), num_perms, alpha))
 
-        results_f.write('\nDM1\tDM2\tClass index\tNum dists\tMantel stat\tp-val\t'
-            'p-val (Bonferroni corrected)\n')
+        results_f.write('\nDM1\tDM2\tClass index\tNum dists\tMantel '
+                        'stat\tp-val\tp-val (Bonferroni corrected)\n')
 
         for i,fp1 in enumerate(input_dm_fps):
             for fp2 in input_dm_fps[i+1:]:
 
-                (dm1_labels, dm1), (dm2_labels, dm2) = make_compatible_distance_matrices(
+                (dm1_labels, dm1), (dm2_labels, dm2) = \
+                    make_compatible_distance_matrices(
                     parse_distmat(open(fp1, 'U')),
                     parse_distmat(open(fp2, 'U')), lookup=sample_id_map)
                 if len(dm1_labels) < 3:
-                    option_parser.error("The distance matrices were not large enough "
-                        "after filtering them to include only samples that were in both "
-                        "matrices. The minimum required size to compute a Mantel "
-                        "correlogram is 3x3.")
+                    option_parser.error("The distance matrices were not large "
+                                        "enough after filtering them to "
+                                        "include only samples that were in "
+                                        "both matrices. The minimum required "
+                                        "size to compute a Mantel "
+                                        "correlogram is 3x3.")
 
                 # Construct a MantelCorrelogram object and run the analysis.
                 results = MantelCorrelogram(DistanceMatrix(dm1, dm1_labels,
@@ -275,24 +346,30 @@ def main():
                 # Write the correlogram plot to a file.
                 dm1_name = '.'.join(fp1.split('.txt')[:-1])
                 dm2_name = '.'.join(fp2.split('.txt')[:-1])
-                fig_file_name = '_'.join((dm1_name, 'VS', dm2_name, 'mantel_correlogram'))+'.'+opts.image_type
+                fig_file_name = '_'.join((dm1_name, 'VS', dm2_name,
+                                    'mantel_correlogram'))+'.'+opts.image_type
                 results['correlogram_plot'].savefig(path.join(opts.output_dir, fig_file_name), format=opts.image_type)
 
                 # Iterate over the results and write them to the text file.
 
                 dm_name_str = '%s\t%s\t' % (fp1,fp2)
 
-                for class_idx, num_dist, r, p, p_corr in zip(results['class_index'], results['num_dist'], results['mantel_r'], results['mantel_p'], results['mantel_p_corr']):
+                for class_idx, num_dist, r, p, p_corr in \
+                        zip(results['class_index'], results['num_dist'],
+                            results['mantel_r'], results['mantel_p'],
+                            results['mantel_p_corr']):
 
                     if p is not None:
                         p_str = format_p_value_for_num_iters(p, num_perms)
                     else:
                         p_str = None
                     if p_corr is not None:
-                        p_corr_str = format_p_value_for_num_iters(p_corr, num_perms)
+                        p_corr_str = format_p_value_for_num_iters(p_corr,
+                                                                  num_perms)
                     else:
                         p_corr_str = None
-                    results_f.write('%s%s\t%d\t%s\t%s\t%s\n' % (dm_name_str, class_idx, num_dist, r, p, p_corr))
+                    results_f.write('%s%s\t%d\t%s\t%s\t%s\n' % (dm_name_str,
+                                    class_idx, num_dist, r, p, p_corr))
                     dm_name_str = '\t\t'
 
 
