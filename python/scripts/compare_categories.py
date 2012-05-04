167f293c39784dd7ca34cdf031c7b365fd27c799
Logan Knecht <loganknecht@gmail.com>
2012-05-04 10:59:31 +0000

    Ooookay, I'm pretty tired so this may be a bunch of rambling. I have put this thing through the steps and it is pycogent compatible. That includes all the weird little nuances like making sure words are plural for things that are lists, arrays, whatever basically containers. 80 character max per a line fix. Didn't have to worry about method or property names. I could go on. It works.
    There is a ton of documentation that has been added. It still doesn't have script usage added, but I'm just so tired right now that I'm going to pass out if I continue.
    
    Best and multicola still aren't in here. This isn't a big deal because best hasn't been unit tested and multicola... well it needs to be looked into more. 
    
    THERE ARE STILL DISCREPANCIES WITH DFA AND PERMANOVA OUTPUT. THIS WILL BE LOOKED INTO.

--- python/scripts/compare_categories.py
+++ python/scripts/compare_categories.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # File created on 22 Apr 2012
+"""
+This is a file that aggregates the compare category methods and provides \
+a consistent interface for using these statistical methods. 
+"""
 from __future__ import division
 
-__author__ = "Michael Dwan"
+__author__ = "Logan Knecht"
 __copyright__ = "Copyright 2012, The QIIME MiCOS project"
 __credits__ = ["Logan Knecht, Michael Dwan"]
 __license__ = "GPL"
@@ -11,10 +15,10 @@ __maintainer__ = "Logan Knecht"
 __email__ = "lgk7@nau.edu, mdwan.tgen@gmail.com"
 __status__ = "Development"
 
+from os import path, makedirs, listdir
+
 from numpy import zeros
 from numpy.random import permutation
-
-from os import path, makedirs, listdir
 
 from cogent.util.misc import create_dir
 
@@ -23,27 +27,96 @@ from qiime.util import parse_command_line_parameters, make_option
 from qiime.parse import parse_distmat, fields_to_dict, \
                         parse_mapping_file, parse_mapping_file_to_dict
 
-#from python.qiime.parse import DistanceMatrix, MetadataMap
-#from python.qiime.r_executor import RExecutor
-from parse import DistanceMatrix, MetadataMap
-from r_executor import RExecutor
+from python.qiime.parse import DistanceMatrix, MetadataMap
+from python.qiime.r_executor import RExecutor
 
-#from python.qiime.stats import Anosim
-from stats import Anosim, Permanova
-#anosim_p_test, _format_anosim_results
+from python.qiime.stats import Anosim, Permanova
 
 script_info = {}
-script_info['brief_description'] = ""
-script_info['script_description'] = ""
-script_info['script_usage'] = [("","","")]
+script_info['brief_description'] = """
+Analyzes distance matrices for information using statistical methods
+"""
+script_info['script_description'] = """
+This script allows for the anaylsis of distance matrices, (in the case of the \
+dfa method it's an otu table), using several statistical methods. These methods\
+ are Adonis, Anosim, BEST, DFA, ISA(not implemented), LSA(not implemented), \
+Moran's I, MRPP, Multicola(not implemented), PERMANOVA, PERMDISP, RDA, and \
+Repeated Measures PERMANOVA(not implemented).
+
+Adonis - This method takes a distance matrix and mapping file. It then \
+identifies important points in the data and performs F-tests on the initial \
+data, and random permutations(via  shuffling) the category data. Then, \
+it finally returns the information that was identified in the samples. It's \
+stated that it partitions (or seperates the data) for this analysis in order\
+ to find underlying relationship.
+
+Anosim - This method takes in a distance matrix and a mapping file. \
+This method tests whether two or more categories are significantly \
+different. You can specify a category in the metadata mapping file to separate \
+samples into groups and then test whether there are significant differences \
+between those groups. For example, you might test whether Control samples are \
+significantly different from Fast samples. Since ANOSIM is non-parametric, \
+significance is determined through permutations.
+
+BEST - This method looks at the numerical environmental variables relating \
+samples in a distance matrix. For instance, the unifrac distance matrix and \
+pH and latitude (or any other number of variables) in soil samples, and ranks \
+them in order of which best explain patterns in the communities.
+
+DFA - This method takes in an OTU table and a mapping file. This method \
+is heavily related to ANOVA/MANOVA, except that it operates in the opposite \
+direction. ANOVA/MANOVA test whether categorical independent \
+variable(s) effectively predict continuous dependent variable(s). On the other \
+hand, DFA tests how well one or more independent continuous variables predicts \
+a categorical dependent variable. In a nutshell, DFA seeks to answer the \
+question 'Is this set of variables effective at predicting category/group \
+membership?'. When applied to ecology, DFA can answer the question 'Are these \
+species/OTUs effective at predicting category/group membership?' or 'What is \
+the best combination of species/OTUs for predicting category/group membersip?'
+
+Moran's I - This method takes in a distance matrix and mapping file. Then it \
+uses the geographical data to identify what type of spatial configuration the \
+samples have. Are they dispersed, clustered, or of no distinctly noticeable \
+configuration when compared to each other?
+
+MRPP - This method takes in a distance matrix and a mapping file. It then \
+tests whether two or more categories are significantly different. You can \
+specify a category in the metadata mapping file to separate samples into \
+groups and then test whether there are significant differences between those \
+groups. For example, you might test whether Control samples are significantly \
+different from Fast samples. Since MRPP is non-parametric, significance is \
+determined through permutations.
+
+PERMANOVA - This method takes distance matrix and a mapping file. This method \
+is for testing the simultaneous response of one or more variables to one or \
+more factors in an ANOVA experimental design on the basis of any distance \
+metric. It returns a R value and a P value. The first thing it does is \
+calculate the distances between each pair of sampled units to obtain a \
+distance matrix. It then calculates the test-statistics from this according \
+to the relevant experimental design.
+
+PERMDISP - This method takes a distance matrix and a mapping file. \
+This method is a procedure for the analysis of multivariate homogeneity of \
+group dispersions (variances). Permutations can be utilized to measure the \
+dissimilatities between groups.
+
+RDA - This method takes a distance matrix and a mapping file. This method \
+is an ordination method that shows grouping/clustering of samples based on \
+a category in the metadata mapping file and a distance matrix. This category \
+is used to explain the variability between samples. Thus, RDA is similar to \
+PCoA except that it is constrained, while PCoA is unconstrained (you must \
+specify which category should be used to explain the variability in your data).
+"""
+script_info['script_usage'] = []
 script_info['output_description']= ""
-#TODO THIS IS BROKEN AND WON'T FAIL WHEN A REQUIRED OPTION ISN'T PASSED IN
 script_info['required_options'] = [\
  # All methods use these
 make_option('--method', help='The category analysis method. Valid options: \
     [adonis, anosim, bioenv, dfa, isa, lsa, morans_i, mrpp, multicola, \
     permanova, permdisp, rda, rm_permanova]'),\
- make_option('-i','--input_dm',help='This should be a distance matrix that is being passed in,  unless the method being performed is DFA. If that is the case the DFA method requires that an otu table be passed in instead.'),\
+ make_option('-i','--input_dm',help='This should be a distance matrix that is \
+being passed in, unless the method being performed is DFA. If that is the case \
+the DFA method requires that an otu table be passed in instead.'),\
  make_option('-o','--output_dir',help='the output directory \
  [default: %default]', default='.'),\
  make_option('-m','--mapping_file', help='Mapping file'),
@@ -59,6 +132,9 @@ script_info['optional_options'] = [\
 script_info['version'] = __version__
 
 def main():
+"""
+This is the entry point for the script to run.
+"""
     option_parser, opts, args =\
     parse_command_line_parameters(**script_info)
 
@@ -85,82 +161,77 @@ def main():
     first_category = categories[0]
     
     #cursory check to make sure all categories passed in are in mapping file
-    map_list = parse_mapping_file(open(opts.mapping_file,'U').readlines())
+    maps = parse_mapping_file(open(opts.mapping_file,'U').readlines())
     for category in categories:
-        if not category in map_list[1][1:]:
+        if not category in maps[1][1:]:
             print "Category '%s' not found in mapping file columns:" % category
-            print map_list[1][1:]
+            print maps[1][1:]
             exit(1)
-
     if opts.method == 'adonis':
-        command_args = ["-d " + opts.input_dm + " -m " + opts.mapping_file + " -c " +\
-            first_category + " -o " + opts.output_dir]
+        command_args = ["-d " + opts.input_dm + " -m " + opts.mapping_file + \
+            " -c " + first_category + " -o " + opts.output_dir]
         rex = RExecutor()
-        results = rex(command_args, "adonis.r", \
-            output_dir=opts.output_dir)
+        rex(command_args, "adonis.r", output_dir=opts.output_dir)
     elif opts.method == 'anosim':
-        anosim_object = Anosim(md_map, dm, first_category)
-        analysisOutput = anosim_object(opts.num_permutations)
+        #runs anosim
+        anosim = Anosim(md_map, dm, first_category)
+        anosim_results = anosim(opts.num_permutations)
         #anosim has been run, now writing results to file
-        outputFile = open(opts.output_dir + "/" + opts.method + \
+        output_file = open(opts.output_dir + "/" + opts.method + \
             "_results.xt","w+")
-        outputFile.write("Method Name:\tR-value:\tP-value:")
-        outputFile.write("\n")
-        outputFile.write(analysisOutput["method_name"]+"\t"+\
-            str(analysisOutput["r_value"])+"\t"+\
-            str(analysisOutput["p_value"])+"\t")
-        outputFile.write("\n")
-        outputFile.close()
+        output_file.write("Method Name:\tR-value:\tP-value:")
+        output_file.write("\n")
+        output_file.write(anosim_results["method_name"]+"\t"+\
+            str(anosim_results["r_value"])+"\t"+\
+            str(anosim_results["p_value"])+"\t")
+        output_file.write("\n")
+        output_file.close()
     elif opts.method == 'best':
         pass
     elif opts.method == 'dfa':
-        command_args = ["-i " + opts.input_dm + " -m " + opts.mapping_file + " -c " +\
-            first_category + " -o " + opts.output_dir]
+        command_args = ["-i " + opts.input_dm + " -m " + opts.mapping_file + \
+            " -c " + first_category + " -o " + opts.output_dir]
         rex = RExecutor()
-        results = rex(command_args, "dfa.r", \
-            output_dir=opts.output_dir)
+        rex(command_args, "dfa.r", output_dir=opts.output_dir)
     elif opts.method == 'morans_i':
-        command_args = ["-i " + opts.input_dm + " -m " + opts.mapping_file + " -c " +\
-            first_category + " -o " + opts.output_dir]
+        command_args = ["-i " + opts.input_dm + " -m " + opts.mapping_file + \
+            " -c " + first_category + " -o " + opts.output_dir]
         rex = RExecutor()
-        results = rex(command_args, "morans_i.r", \
-            output_dir=opts.output_dir)
+        rex(command_args, "morans_i.r", output_dir=opts.output_dir)
     elif opts.method == 'mrpp':
-        command_args = ["-d " + opts.input_dm + " -m " + opts.mapping_file + " -c " +\
-            first_category + " -o " + opts.output_dir]
+        command_args = ["-d " + opts.input_dm + " -m " + opts.mapping_file + \
+            " -c " + first_category + " -o " + opts.output_dir]
         rex = RExecutor()
-        results = rex(command_args, "mrpp.r", \
-            output_dir=opts.output_dir)
+        rex(command_args, "mrpp.r", output_dir=opts.output_dir)
     elif opts.method == 'multicola':
         pass
     elif opts.method == 'permanova':
         #makes a permanova object
         permanova_plain = Permanova(md_map, dm, first_category)
         #relies on the __call__ property and
-        results = permanova_plain(opts.num_permutations)
+        permanova_results = permanova_plain(opts.num_permutations)
         #writes the results to the output dir
-        outputFile = open(opts.output_dir+"/permanova_results.txt", 'w+')
-        outputFile.write("Method Name:\tR-value:\tP-value:")
-        outputFile.write("\n")
-        outputFile.write(results["method_name"]+"\t"+str(results["r_value"])+\
-            "\t"+format_p_value_for_num_iters(results["p_value"], \
+        output_file = open(opts.output_dir+"/permanova_results.txt", 'w+')
+        output_file.write("Method Name:\tR-value:\tP-value:")
+        output_file.write("\n")
+        output_file.write(permanova_results["method_name"]+"\t"+\
+            str(permanova_results["r_value"]) + "\t" + \
+            format_p_value_for_num_iters(permanova_results["p_value"], \
             opts.num_permutations)+"\t")
-        outputFile.write("\n")
-        outputFile.close()
+        output_file.write("\n")
+        output_file.close()
     elif opts.method == 'permdisp':
-        command_args = ["-d " + opts.input_dm + " -m " + opts.mapping_file + " -c " +\
-            first_category + " -o " + opts.output_dir]
+        command_args = ["-d " + opts.input_dm + " -m " + opts.mapping_file + \
+            " -c " + first_category + " -o " + opts.output_dir]
         rex = RExecutor()
-        results = rex(command_args, "betadisper.r", output_dir=opts.output_dir)
+        rex(command_args, "betadisper.r", output_dir=opts.output_dir)
     elif opts.method == 'rda':
-        command_args = ["-i " + opts.input_dm + " -m " + opts.mapping_file + " -c " +\
-            first_category + " -o " + opts.output_dir]
+        command_args = ["-i " + opts.input_dm + " -m " + opts.mapping_file + \
+            " -c " + first_category + " -o " + opts.output_dir]
         rex = RExecutor()
-        results = rex(command_args, "rda.r", output_dir=opts.output_dir)
-    elif opts.method == 'rm_permanova':
-        pass
+        rex(command_args, "rda.r", output_dir=opts.output_dir)
     else:
-        print "Method '%s' not recognized"
+        print "Method '%s' not recognized" % opts.method
 
 if __name__ == "__main__":
     main()
