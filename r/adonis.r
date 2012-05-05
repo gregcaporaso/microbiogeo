093f977176765f547b3d86c173235741a0f4e7f8
Jai Ram Rideout <jai.rideout@gmail.com>
2012-05-05 22:06:46 +0000

    Fixed up adonis.r script to match QIIME's randomforests.r.

--- r/adonis.r
+++ r/adonis.r
@@ -1,37 +1,41 @@
-# Runs vegan function ADONIS on QIIME distance file.
+# Runs vegan function adonis on QIIME distance matrix
 # usage:
-# R --slave --args -d unifrac.txt -m Fasting_Map.txt -c Treatment -o adonis < adonis.r
-# 
-# print help string:
-# R --slave --args -h < adonis.r
+# R --slave --args --source_dir $QIIME_HOME/qiime/support_files/R/ -d unifrac.txt -m Fasting_Map.txt -c Treatment -o adonis < adonis.r
 #
-# Requires environment variable QIIME_DIR pointing to  top-level QIIME directory.
+# print help string:
+# R --slave --args -h --source_dir $QIIME_HOME/qiime/support_files/R/ < adonis.r
+#
+# Requires command-line param --source_dir pointing to QIIME R source dir
 
 # load libraries and source files
-library('optparse',warn.conflicts=FALSE,quietly=TRUE)
-library('vegan',warn.conflicts=FALSE,quietly=TRUE)
-envvars <- as.list(Sys.getenv())
-if(is.element('QIIME_DIR', names(envvars))){
-    qiimedir <- envvars[['QIIME_DIR']]
-    source(sprintf('%s/qiime/support_files/R/loaddata.r',qiimedir))
-} else {
-    stop("Please add QIIME_DIR environment variable pointing to the top-level QIIME directory.")
+args <- commandArgs(trailingOnly=TRUE)
+if(!is.element('--source_dir', args)){
+    stop("\n\nPlease use '--source_dir' to specify the R source code directory.\n\n")
 }
+sourcedir <- args[which(args == '--source_dir') + 1]
+source(sprintf('%s/loaddata.r',sourcedir))
+source(sprintf('%s/util.r',sourcedir))
+load.library('optparse')
+load.library('vegan')
 
 # make option list and parse command line
 option_list <- list(
+    make_option(c("--source_dir"), type="character",
+        help="Path to R source directory [required]."),
     make_option(c("-d", "--distmat"), type="character",
         help="Input distance matrix [required]."),
     make_option(c("-m", "--mapfile"), type="character",
         help="Input metadata mapping file [required]."),
     make_option(c("-c", "--category"), type="character",
-        help="Metadata column header giving cluster IDs [required]"),
+        help="Metadata column header giving cluster IDs [required]."),
+    make_option(c("-n", "--num_permutations"), type="integer", default=999,
+        help="Number of permutations [default %default]."),
     make_option(c("-o", "--outdir"), type="character", default='.',
-        help="Output directory [default %default]")
+        help="Output directory [default %default].")
 )
-opts <- parse_args(OptionParser(option_list = option_list), args = commandArgs(trailingOnly=TRUE))
+opts <- parse_args(OptionParser(option_list=option_list), args=args)
 
-# File requirements
+# error checking
 if(is.null(opts$mapfile)) stop('Please supply a mapping file.')
 if(is.null(opts$category)) stop('Please supply a mapping file header.')
 if(is.null(opts$distmat)) stop('Please supply a distance matrix.')
@@ -45,7 +49,9 @@ distmat <- load.qiime.distance.matrix(opts$distmat)
 qiime.data <- remove.nonoverlapping.samples(map=map, distmat=distmat)
 
 # run adonis
-results <- adonis(formula = as.dist(qiime.data$distmat) ~ qiime.data$map[[opts$category]])
+results <- adonis(
+    formula = as.dist(qiime.data$distmat) ~ qiime.data$map[[opts$category]],
+    permutations = opts$num_permutations)
 
 # write output file
 filepath <- sprintf('%s/adonis_results.txt',opts$outdir)
