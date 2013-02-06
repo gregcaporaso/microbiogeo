82b8ee39f383d1b9f6a155e980137316dd3dcca4
Jai Ram Rideout <jai.rideout@gmail.com>
2013-02-06 16:58:43 -0700

    Added tests for subset_dm().

--- code/tests/test_util.py
+++ code/tests/test_util.py
@@ -63,6 +63,26 @@ class UtilTests(TestCase):
         # XOR: either S1 or S3 should be in obs_labels, but not both.
         self.assertTrue(('S1' in obs_labels) != ('S3' in obs_labels))
 
+    def test_subset_dm(self):
+        """Test picking a subset of a distance matrix."""
+        # Don't actually subset.
+        exp = parse_distmat(self.dm_f1)
+        obs = parse_distmat(subset_dm(self.dm_f1, 3).split('\n'))
+        self.assertFloatEqual(obs, exp)
+
+        obs_labels, obs_dm = parse_distmat(
+                subset_dm(self.dm_f1, 1).split('\n'))
+        self.assertEqual(len(obs_labels), 1)
+        self.assertTrue(obs_labels[0] in exp[0])
+
+        obs_labels, obs_dm = parse_distmat(
+                subset_dm(self.dm_f1, 2).split('\n'))
+        self.assertEqual(len(obs_labels), 2)
+        self.assertTrue(obs_labels[0] in exp[0])
+        self.assertTrue(obs_labels[1] in exp[0])
+
+        self.assertRaises(ValueError, subset_dm, self.dm_f1, 4)
+
 
 dm_str1 = """\tS1\tS2\tS3
 S1\t0\t0.5\t0.7
