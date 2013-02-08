aa93aca1250f5cfac4e50252ed72b30ed6040f1d
Jai Ram Rideout <jai.rideout@gmail.com>
2013-02-08 16:32:01 -0700

    Refactored heatmap code out of notebooks.
    Some unit tests in place, more to come.

--- code/microbiogeo/format.py
+++ code/microbiogeo/format.py
@@ -11,13 +11,21 @@ __email__ = "jai.rideout@gmail.com"
 
 """Module to format data for presentation."""
 
+from collections import defaultdict
 from csv import writer
 from os.path import join
 
+from numpy import ones
+
+from matplotlib.pyplot import (colorbar, figure, imshow, matshow, savefig,
+                               subplot, tight_layout, xticks, yticks)
+
+from cogent.maths.stats.test import pearson, spearman
+
 # Not unit-tested.
 def create_results_summary_tables(results, out_dir, filename_prefix):
-    """Create tables to summarize the results of the statistical methods.
-    
+    """Creates tables to summarize the results of the statistical methods.
+
     These tables will be in TSV format so that they can be easily imported into
     Excel for viewing and cleanup for publication.
 
@@ -101,3 +109,111 @@ def format_p_value_as_asterisk(p_value):
         result += '*'
 
     return result
+
+# Not unit-tested.
+def create_method_comparison_heatmaps(results, methods, method_labels, out_dir,
+                                      filename_prefix):
+    """Generates heatmaps showing the correlation between each pair of methods.
+
+    Generates two heatmaps (one for Pearson correlation, one for Spearman
+    correlation). Uses all available results (e.g. all even sampling depths,
+    metrics, and datasets) that match between each pair of methods as input to
+    the correlation coefficient methods.
+
+    Each heatmap will be written to out_dir with the filename convention
+    <filename_prefix>_<correlation_method>.pdf.
+    """
+    for correlation_method, heatmap_data in \
+            format_method_comparison_heatmaps(results, methods):
+        # Generate the heatmap. Code based on
+        # http://matplotlib.org/users/tight_layout_guide.html and
+        # http://psaffrey.wordpress.com/2010/07/05/chromosome-interactions-
+        #   heatmaps-and-matplotlib/
+        fig = figure()
+        ax = subplot(111)
+        im = ax.matshow(heatmap_data, vmin=0, vmax=1)
+
+        colorbar(im, use_gridspec=True)
+        xticks(range(len(method_labels)), method_labels, rotation=90)
+        yticks(range(len(method_labels)), method_labels)
+
+        tight_layout()
+        savefig('%s_%s.pdf' % (filename_prefix, correlation_method),
+                format='pdf')
+
+def format_method_comparison_heatmaps(results, methods):
+    shared_studies = None
+    shared_categories = {}
+
+    for depth_desc, depth_res in results.items():
+        for metric, metric_res in depth_res.items():
+            for method, method_res in metric_res.items():
+                if method in methods:
+                    studies = sorted(method_res.keys())
+
+                    if shared_studies is None:
+                        shared_studies = studies
+                    elif studies != shared_studies:
+                        raise ValueError("Not all methods to include in the "
+                                         "heatmap have results for the same "
+                                         "studies.")
+
+                    for study, study_res in sorted(method_res.items()):
+                        categories = [category for category, category_res in
+                                      sorted(study_res.items())
+                                      if len(category_res) > 0]
+                        if study not in shared_categories:
+                            shared_categories[study] = set(categories)
+                        else:
+                            shared_categories[study] &= set(categories)
+
+    # Gather all test statistics for each method (in the same order for each
+    # method!).
+    method_data = defaultdict(list)
+    for depth_desc, depth_res in results.items():
+        for metric, metric_res in depth_res.items():
+            for method, method_res in metric_res.items():
+                if method in methods:
+                    for study, study_res in sorted(method_res.items()):
+                        for category, category_res in sorted(
+                                study_res.items()):
+                            if category in shared_categories[study]:
+                                method_data[method].append(
+                                        category_res['full'][0])
+                                method_data[method].append(
+                                        category_res['shuffled'][0])
+                                
+                                for es, p_vals in zip(
+                                        *category_res['subsampled']):
+                                    method_data[method].append(es)
+
+    # Make sure our data looks sane. We should have the same number of
+    # observations for each method.
+    data_length = None
+    for method, data in method_data.items():
+        if data_length is None:
+            data_length = len(data)
+        elif len(data) != data_length:
+            raise ValueError("The number of test statistics is not the same "
+                             "between all methods, so we can't compare them.")
+
+    # Compute the correlation coefficient between each pair of methods and put
+    # the output in an array. This array can then be used to generate a
+    # text-based table or heatmap.
+    heatmaps = {}
+    for correlation_name, correlation_fn in \
+            ('pearson', pearson), ('spearman', spearman):
+        num_methods = len(methods)
+        heatmap_data = ones((num_methods, num_methods))
+
+        # I know this is inefficient, but it really doesn't matter for what
+        # we're doing here.
+        for method1_idx, method1 in enumerate(methods):
+            for method2_idx, method2 in enumerate(methods):
+                corr_coeff = correlation_fn(method_data[method1],
+                                            method_data[method2])
+                heatmap_data[method1_idx][method2_idx] = corr_coeff
+
+        heatmaps[correlation_name] = heatmap_data
+
+    return heatmaps
--- code/tests/test_format.py
+++ code/tests/test_format.py
@@ -11,9 +11,12 @@ __email__ = "jai.rideout@gmail.com"
 
 """Test suite for the format.py module."""
 
+from numpy import array
+
 from cogent.util.unit_test import TestCase, main
 
-from microbiogeo.format import (format_method_comparison_table,
+from microbiogeo.format import (format_method_comparison_heatmaps,
+                                format_method_comparison_table,
                                 format_p_value_as_asterisk)
 
 class FormatTests(TestCase):
@@ -58,10 +61,62 @@ class FormatTests(TestCase):
             }
         }
 
+        self.full_results1 = {
+            '5_percent': {
+                'unweighted_unifrac': {
+                    'adonis': {
+                        'whole_body': {
+                            'BODY_SITE': {
+                                'full': (0.27, [0.01, 0.001]),
+                                'shuffled': (0.02, [0.45, 0.476]),
+                                'subsampled': ([0.24, 0.20], [[0.03, 0.005],
+                                                              [0.02, 0.023]])
+                            }
+                        },
+
+                        '88_soils': {
+                            'ENV_BIOME': {
+                                'full': (0.13, [0.02, 0.002]),
+                                'shuffled': (0.03, [0.40, 0.401]),
+                                'subsampled': ([0.22, 0.19], [[0.01, 0.009],
+                                                              [0.06, 0.029]])
+                            }
+                        },
+
+                        'keyboard': {}
+                    },
+
+                    'anosim': {
+                        'whole_body': {
+                            'BODY_SITE': {
+                                'full': (0.27, [0.01, 0.001]),
+                                'shuffled': (0.02, [0.45, 0.476]),
+                                'subsampled': ([0.24, 0.20], [[0.03, 0.005],
+                                                              [0.02, 0.023]])
+                            }
+                        },
+
+                        'keyboard': {
+                            'HOST_SUBJECT_ID': {
+                                'full': (0.59, [0.11, 0.101]),
+                                'shuffled': (0.32, [0.65, 0.776]),
+                                'subsampled': ([0.13, 0.42], [[0.23, 0.105],
+                                                              [0.92, 0.723]])
+                            }
+                        },
+
+                        '88_soils': {
+                            'ENV_BIOME': {}
+                        }
+                    }
+                }
+            }
+        }
+
     def test_format_method_comparison_table(self):
         """Test formatting a methods summary table."""
         obs = format_method_comparison_table(self.per_method_results1)
-        self.assertEqual(obs, exp_method_comparison_table)
+        self.assertEqual(obs, exp_method_comparison_table1)
 
         self.assertRaises(ValueError, format_method_comparison_table,
                           self.per_method_results2)
@@ -95,13 +150,22 @@ class FormatTests(TestCase):
         self.assertRaises(ValueError, format_p_value_as_asterisk, 1.1)
         self.assertRaises(ValueError, format_p_value_as_asterisk, -0.042)
 
+    def test_format_method_comparison_heatmaps(self):
+        obs = format_method_comparison_heatmaps(self.full_results1,
+                                                ['adonis', 'anosim'])
+        self.assertEqual(obs, exp_method_comparison_heatmaps1)
 
-exp_method_comparison_table = [['Method', 'whole_body\rBODY_SITE',
+
+exp_method_comparison_table1 = [['Method', 'whole_body\rBODY_SITE',
                                 'whole_body\rBODY_SITE (shuffled)',
                                 'whole_body\rBODY_SITE (subsampled)'],
                                ['adonis', '0.27; ***, ****', '0.02; x, x',
                                 '0.24; **, ***\r0.20; **, **'],
                                ['anosim', ['N/A', 'N/A', 'N/A']]]
+
+exp_method_comparison_heatmaps1 = {'spearman': array([[ 1.,  1.],
+                                   [ 1.,  1.]]), 'pearson': array([[ 1.,  1.],
+                                   [ 1.,  1.]])}
 
 
 if __name__ == "__main__":
