c71101f28d09b3fcc2b46fe65a19a73c97008b24
Jai Ram Rideout <jai.rideout@gmail.com>
2013-03-26 11:23:19 -0700

    Added subset chooser for gradient data.

--- code/microbiogeo/simulate.py
+++ code/microbiogeo/simulate.py
@@ -13,12 +13,12 @@ __email__ = "jai.rideout@gmail.com"
 
 from biom.parse import parse_biom_table
 from collections import defaultdict
-from numpy import inf
-from random import sample
-
+from numpy import ceil, inf
 from qiime.filter import (filter_mapping_file_from_mapping_f,
                           filter_samples_from_otu_table)
+from qiime.parse import parse_mapping_file_to_dict
 from qiime.util import MetadataMap
+from random import randint, sample
 
 def choose_cluster_subsets(otu_table_f, map_f, category,
                            num_samples_per_group):
@@ -40,3 +40,45 @@ def choose_cluster_subsets(otu_table_f, map_f, category,
     return (filter_samples_from_otu_table(otu_table, samp_ids_to_keep, 0, inf),
             filter_mapping_file_from_mapping_f(map_f, samp_ids_to_keep),
             len(samp_ids_to_keep))
+
+def choose_gradient_subsets(otu_table_f, map_f, gradient, num_samples):
+    otu_table = parse_biom_table(otu_table_f)
+    mdm, _ = parse_mapping_file_to_dict(map_f)
+
+    # Only keep the sample IDs that are in both the mapping file and OTU table.
+    samp_ids = [(samp_id, float(metadata[gradient]))
+                for samp_id, metadata in mdm.items()
+                if samp_id in otu_table.SampleIds]
+    samp_ids.sort(key=lambda samp_id: samp_id[1])
+
+    # Adapted from http://stackoverflow.com/a/9873935
+    # We add 1 to the number of samples we want because we want num_samples
+    # intervals to choose from.
+    bin_idxs = [int(ceil(i * len(samp_ids) / (num_samples + 1)))
+                for i in range(num_samples + 1)]
+
+    samp_ids_to_keep = []
+
+    for i in range(len(bin_idxs) - 1):
+        if i == len(bin_idxs) - 2:
+            # We're at the last bin, so choose from the entire bin range.
+            if bin_idxs[i + 1] < len(samp_ids):
+                end_idx = bin_idxs[i + 1]
+            else:
+                end_idx = bin_idxs[i + 1] - 1
+
+            samp_ids_to_keep.append(
+                    samp_ids[randint(bin_idxs[i], end_idx)][0])
+        else:
+            # We subtract 1 since randint is inclusive on both sides, and we
+            # don't want to choose the same sample ID multiple times from
+            # different bins.
+            samp_ids_to_keep.append(
+                    samp_ids[randint(bin_idxs[i],
+                                     bin_idxs[i + 1] - 1)][0])
+
+    assert len(samp_ids_to_keep) == num_samples, \
+           "%d != %d" % (len(samp_ids_to_keep), num_samples)
+
+    return (filter_samples_from_otu_table(otu_table, samp_ids_to_keep, 0, inf),
+            filter_mapping_file_from_mapping_f(map_f, samp_ids_to_keep))
